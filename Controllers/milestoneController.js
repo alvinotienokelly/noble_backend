7b26b141622ad7d3fd3ac446260b0b9a58bbc94f
Alvin <solutions@Alvin.local>
2024-11-13 12:16:42 +0300

    Update milestone

--- Controllers/milestoneController.js
+++ Controllers/milestoneController.js
@@ -46,6 +46,8 @@ const updateMilestone = async (req, res) => {
         .json({ status: false, message: "Milestone not found." });
     }
 
+    
+
     await milestone.update(req.body);
     res.status(200).json({ status: true, milestone });
   } catch (error) {
