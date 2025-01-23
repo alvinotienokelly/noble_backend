66a8c31937d8851eb6e47f6cafc567b83612b9fb
Alvin <solutions@Alvin.local>
2025-01-23 12:30:49 +0300

    pipeline stages
    get pipeline stage associate with pipeline in include

--- Controllers/pipelineStageController.js
+++ Controllers/pipelineStageController.js
@@ -1,6 +1,7 @@
 // Controllers/pipelineStageController.js
 const db = require("../Models");
 const PipelineStage = db.pipeline_stages;
+const Pipeline = db.pipelines;
 
 // Create a new pipeline stage
 const createPipelineStage = async (req, res) => {
@@ -10,6 +11,26 @@ const createPipelineStage = async (req, res) => {
     const pipelineStage = await PipelineStage.create({ name, pipeline_id });
 
     res.status(201).json({ status: true, pipelineStage });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+// Get pipeline stages by pipeline ID
+const getPipelineStagesByPipelineId = async (req, res) => {
+  try {
+    const { pipeline_id } = req.params;
+    const pipelineStages = await PipelineStage.findAll({
+      where: { pipeline_id },
+      include: [{ model: Pipeline, as: "pipeline" }],
+    });
+    if (!pipelineStages || pipelineStages.length === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "No pipeline stages found for this pipeline.",
+      });
+    }
+    res.status(200).json({ status: true, pipelineStages });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -30,7 +51,9 @@ const getPipelineStageById = async (req, res) => {
   try {
     const pipelineStage = await PipelineStage.findByPk(req.params.id);
     if (!pipelineStage) {
-      return res.status(404).json({ status: false, message: "Pipeline stage not found." });
+      return res
+        .status(404)
+        .json({ status: false, message: "Pipeline stage not found." });
     }
     res.status(200).json({ status: true, pipelineStage });
   } catch (error) {
@@ -43,7 +66,9 @@ const updatePipelineStage = async (req, res) => {
   try {
     const pipelineStage = await PipelineStage.findByPk(req.params.id);
     if (!pipelineStage) {
-      return res.status(404).json({ status: false, message: "Pipeline stage not found." });
+      return res
+        .status(404)
+        .json({ status: false, message: "Pipeline stage not found." });
     }
     await pipelineStage.update(req.body);
     res.status(200).json({ status: true, pipelineStage });
@@ -57,10 +82,14 @@ const deletePipelineStage = async (req, res) => {
   try {
     const pipelineStage = await PipelineStage.findByPk(req.params.id);
     if (!pipelineStage) {
-      return res.status(404).json({ status: false, message: "Pipeline stage not found." });
+      return res
+        .status(404)
+        .json({ status: false, message: "Pipeline stage not found." });
     }
     await pipelineStage.destroy();
-    res.status(200).json({ status: true, message: "Pipeline stage deleted successfully." });
+    res
+      .status(200)
+      .json({ status: true, message: "Pipeline stage deleted successfully." });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -72,4 +101,5 @@ module.exports = {
   getPipelineStageById,
   updatePipelineStage,
   deletePipelineStage,
+  getPipelineStagesByPipelineId,
 };
--- Routes/pipelineStageRoutes.js
+++ Routes/pipelineStageRoutes.js
@@ -1,7 +1,14 @@
 // Routes/pipelineStageRoutes.js
 const express = require("express");
 const pipelineStageController = require("../Controllers/pipelineStageController");
-const { createPipelineStage, getAllPipelineStages, getPipelineStageById, updatePipelineStage, deletePipelineStage } = pipelineStageController;
+const {
+  createPipelineStage,
+  getAllPipelineStages,
+  getPipelineStageById,
+  updatePipelineStage,
+  deletePipelineStage,
+  getPipelineStagesByPipelineId,
+} = pipelineStageController;
 const authMiddleware = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
@@ -11,5 +18,10 @@ router.get("/", authMiddleware, getAllPipelineStages);
 router.get("/:id", authMiddleware, getPipelineStageById);
 router.put("/:id", authMiddleware, updatePipelineStage);
 router.delete("/:id", authMiddleware, deletePipelineStage);
+router.get(
+  "/pipeline/:pipeline_id",
+  authMiddleware,
+  getPipelineStagesByPipelineId
+);
 
 module.exports = router;
