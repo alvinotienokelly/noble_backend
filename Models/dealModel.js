3171624f0dbac35110230806787b25faa5e2b143
Alvin <solutions@Alvin.local>
2025-01-31 06:30:02 +0300

    deal archived status

--- Models/dealModel.js
+++ Models/dealModel.js
@@ -47,6 +47,7 @@ module.exports = (sequelize, DataTypes) => {
           "Inactive",
           "Closed",
           "Closed & Reopened",
+          "Archived", // Add Archived status
         ],
         allowNull: false,
         defaultValue: "Active",
---
+++ migrations/20250131032744-add-archived-to-deal-status.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      ALTER TYPE "enum_deals_status" ADD VALUE 'Archived';
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Note: Removing a value from an ENUM type is not straightforward in PostgreSQL.
+    // You may need to recreate the ENUM type without the 'Archived' value if necessary.
+  },
+};
