552dfc2c4e69a14fcdfec1d78ae0d36939cfa83b
Alvin <solutions@Alvin.local>
2025-02-03 11:11:29 +0300

    Bulk Upload for sectors and subsectors

--- Routes/sectorRoutes.js
+++ Routes/sectorRoutes.js
@@ -1,8 +1,16 @@
 // Routes/sectorRoutes.js
 const express = require("express");
 const sectorController = require("../Controllers/sectorController");
-const { getAllSectors, getSectorById, createSector, updateSector, deleteSector } = sectorController;
+const {
+  getAllSectors,
+  getSectorById,
+  createSector,
+  updateSector,
+  deleteSector,
+  bulkUploadSectorsAndSubsectors,
+} = sectorController;
 const authMiddleware = require("../Middlewares/authMiddleware");
+const upload = require("../Middlewares/bulkUpload");
 
 const router = express.Router();
 
@@ -11,5 +19,6 @@ router.get("/:id", authMiddleware, getSectorById);
 router.post("/", authMiddleware, createSector);
 router.put("/:id", authMiddleware, updateSector);
 router.delete("/:id", authMiddleware, deleteSector);
+router.post("/bulk-upload", authMiddleware, upload.single("file"), bulkUploadSectorsAndSubsectors); // Add this line
 
 module.exports = router;
