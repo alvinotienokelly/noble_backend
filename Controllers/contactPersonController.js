b3f1cbb75bde8f7f1c129c568941cc2469fd441a
Alvin <solutions@Alvin.local>
2025-02-18 12:31:57 +0300

    create contact person notification

--- Controllers/contactPersonController.js
+++ Controllers/contactPersonController.js
@@ -2,6 +2,7 @@
 const db = require("../Models");
 const ContactPerson = db.contact_persons;
 const { createAuditLog } = require("./auditLogService");
+const { createNotification } = require("./notificationController");
 
 const createContactPerson = async (req, res) => {
   try {
@@ -25,6 +26,11 @@ const createContactPerson = async (req, res) => {
       // entityId: contactPerson.id,
       ip_address: req.ip,
     });
+    await createNotification(
+      user_id,
+      "Create contact person",
+      "Your  contact person Created."
+    );
 
     res.status(201).json({ status: true, contactPerson });
   } catch (error) {
