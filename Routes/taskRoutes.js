d769696edf23cd234b15ee8dadd5d01686525fd9
Alvin <solutions@Alvin.local>
2025-02-20 14:29:25 +0300

    Get user deal tasks

--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -14,6 +14,7 @@ const {
   assignTaskToUser,
   changeTaskStatus,
   getTasksForUserDeals,
+  getUserTasksByDealId,
 } = taskController;
 const authMiddleware = require("../Middlewares/authMiddleware");
 
