05e9dfe16ee5147a33901a01c0e375183eae2d31
Alvin Odiwuor <alvinodiwuor@webmasters.co.ke>
2025-03-13 11:26:43 +0300

    UUI cleanup

--- seeders/20250109080956-seed-roles.js
+++ seeders/20250109080956-seed-roles.js
@@ -3,11 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('roles', [
-      { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Administrator', createdAt: new Date(), updatedAt: new Date() },
-      { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Investor', createdAt: new Date(), updatedAt: new Date() },
-      { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Target Company', createdAt: new Date(), updatedAt: new Date() },
-    ], {});
+    // await queryInterface.bulkInsert('roles', [
+    //   { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Administrator', createdAt: new Date(), updatedAt: new Date() },
+    //   { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Investor', createdAt: new Date(), updatedAt: new Date() },
+    //   { role_id: Sequelize.literal('uuid_generate_v4()'), name: 'Target Company', createdAt: new Date(), updatedAt: new Date() },
+    // ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20250210073608-seed-document-types.js
+++ seeders/20250210073608-seed-document-types.js
@@ -6,19 +6,19 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("document_types", [
       {
-        type_id: uuidv4(),
+        type_id: Sequelize.literal("uuid_generate_v4()"),
         name: "Information Memorandum",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        type_id: uuidv4(),
+        type_id: Sequelize.literal("uuid_generate_v4()"),
         name: "NDA",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        type_id: uuidv4(),
+        type_id: Sequelize.literal("uuid_generate_v4()"),
         name: "Financial Model",
         createdAt: new Date(),
         updatedAt: new Date(),
