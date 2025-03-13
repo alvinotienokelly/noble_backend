282addff54643960c88e94591f5c4b8bbc37244d
Alvin Odiwuor <alvinodiwuor@webmasters.co.ke>
2025-03-13 10:57:25 +0300

    Capture target company value

--- seeders/20250109080956-seed-permissions.js
+++ seeders/20250109080956-seed-permissions.js
@@ -1,17 +1,41 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // await queryInterface.bulkInsert('permissions', [
-    //   { permission_id: Sequelize.literal('uuid_generate_v4()'), name: 'CREATE_DEAL', createdAt: new Date(), updatedAt: new Date() },
-    //   { permission_id: Sequelize.literal('uuid_generate_v4()'), name: 'UPDATE_DEAL', createdAt: new Date(), updatedAt: new Date() },
-    //   { permission_id: Sequelize.literal('uuid_generate_v4()'), name: 'DELETE_DEAL', createdAt: new Date(), updatedAt: new Date() },
-    //   { permission_id: Sequelize.literal('uuid_generate_v4()'), name: 'VIEW_DEAL', createdAt: new Date(), updatedAt: new Date() },
-    // ], {});
+    await queryInterface.bulkInsert(
+      "permissions",
+      [
+        {
+          permission_id: Sequelize.literal("uuid_generate_v4()"),
+          name: "CREATE_DEAL",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          permission_id: Sequelize.literal("uuid_generate_v4()"),
+          name: "UPDATE_DEAL",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          permission_id: Sequelize.literal("uuid_generate_v4()"),
+          name: "DELETE_DEAL",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          permission_id: Sequelize.literal("uuid_generate_v4()"),
+          name: "VIEW_DEAL",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('permissions', null, {});
+    await queryInterface.bulkDelete("permissions", null, {});
   },
 };
