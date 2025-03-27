76820aafd69d515e01107456528b547e7ded8e9d
Alvin Odiwuor <alvinodiwuor@webmasters.co.ke>
2025-03-27 11:30:35 +0300

    Graph client for meetings

--- Middlewares/graphClient.js
+++ Middlewares/graphClient.js
@@ -1,17 +1,16 @@
-// graphClient.js
 const { Client } = require("@microsoft/microsoft-graph-client");
-const { InteractiveBrowserCredential } = require('@azure/identity');
+const { ClientSecretCredential } = require("@azure/identity");
 
-const credential = new InteractiveBrowserCredential({
-  clientId: process.env.CLIENT_ID,
-  tenantId: process.env.TENANT_ID,
-  clientSecret: process.env.SECRET_ID,
-});
+const tenantId = process.env.TENANT_ID;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
 const graphClient = Client.initWithMiddleware({
   authProvider: {
     getAccessToken: async () => {
-      const tokenResponse = await credential.getToken('https://graph.microsoft.com/.default');
+      const tokenResponse = await credential.getToken("https://graph.microsoft.com/.default");
       return tokenResponse.token;
     },
   },
