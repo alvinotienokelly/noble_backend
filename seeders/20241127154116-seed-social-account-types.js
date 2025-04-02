de86326755c81ee8dabe465b2ea64dce5a00ef19
Alvin Odiwuor <alvinodiwuor@webmasters.co.ke>
2025-04-02 10:32:30 +0300

    Permissions seeder

--- seeders/20241127154116-seed-social-account-types.js
+++ seeders/20241127154116-seed-social-account-types.js
@@ -2,36 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(
-      "social_account_types",
-      [
-        {
-          type_id: Sequelize.literal("uuid_generate_v4()"),
-          name: "X",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          type_id: Sequelize.literal("uuid_generate_v4()"),
-          name: "LinkedIn",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          type_id: Sequelize.literal("uuid_generate_v4()"),
-          name: "Facebook",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          type_id: Sequelize.literal("uuid_generate_v4()"),
-          name: "Instagram",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    // await queryInterface.bulkInsert(
+    //   "social_account_types",
+    //   [
+    //     {
+    //       type_id: Sequelize.literal("uuid_generate_v4()"),
+    //       name: "X",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       type_id: Sequelize.literal("uuid_generate_v4()"),
+    //       name: "LinkedIn",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       type_id: Sequelize.literal("uuid_generate_v4()"),
+    //       name: "Facebook",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       type_id: Sequelize.literal("uuid_generate_v4()"),
+    //       name: "Instagram",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //   ],
+    //   {}
+    // );
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20250210073608-seed-document-types.js
+++ seeders/20250210073608-seed-document-types.js
@@ -4,26 +4,26 @@ const { v4: uuidv4 } = require("uuid");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("document_types", [
-      {
-        type_id: Sequelize.literal("uuid_generate_v4()"),
-        name: "Information Memorandum",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        type_id: Sequelize.literal("uuid_generate_v4()"),
-        name: "NDA",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        type_id: Sequelize.literal("uuid_generate_v4()"),
-        name: "Financial Model",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    // await queryInterface.bulkInsert("document_types", [
+    //   {
+    //     type_id: Sequelize.literal("uuid_generate_v4()"),
+    //     name: "Information Memorandum",
+    //     createdAt: new Date(),
+    //     updatedAt: new Date(),
+    //   },
+    //   {
+    //     type_id: Sequelize.literal("uuid_generate_v4()"),
+    //     name: "NDA",
+    //     createdAt: new Date(),
+    //     updatedAt: new Date(),
+    //   },
+    //   {
+    //     type_id: Sequelize.literal("uuid_generate_v4()"),
+    //     name: "Financial Model",
+    //     createdAt: new Date(),
+    //     updatedAt: new Date(),
+    //   },
+    // ]);
   },
 
   down: async (queryInterface, Sequelize) => {
